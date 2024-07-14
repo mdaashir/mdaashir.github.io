aba2a83202ac739dcbbfed80271649b402fab402
mdaashir <s.mohamedaashir@gmail.com>
2024-07-14 20:02:52 +0530

    Removed and added new navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Blogs from './pages/blogs';
-import SignUp from './pages/signup';
-import Contact from './pages/contact';
+import NavBar from './components/NavBar';
 
 function App() {
 	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route exact path='/' element={<Home />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/contact' element={<Contact />} />
-				<Route path='/blogs' element={<Blogs />} />
-				<Route path='/sign-up' element={<SignUp />} />
-			</Routes>
-		</Router>
+		<div>
+			<NavBar />
+		</div>
 	);
 }
 
