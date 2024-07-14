94f88fb39c50f805838576ec8055274ef28188a9
mdaashir <s.mohamedaashir@gmail.com>
2024-07-14 07:35:58 +0530

    Added basic navbar

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>Mohamed Aashir S</h1>
+			<p>
+				A Tech Enthusiast with a passion for coding. With a background in
+				full-stack web development and native app development, I excel at
+				crafting elegant solutions.
+			</p>
+		</div>
+	);
+};
+
+export default About;
---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+	return (
+		<div>
+			<h1>Mail us s.mohamedaashir@gmail.com</h1>
+		</div>
+	);
+};
+
+export default Contact;
